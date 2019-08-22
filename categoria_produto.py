0eb2b300e580149a7016fb9ef73c0ca9b129803b
Rafael Nilson Witt <34680072+rafaelwitter@users.noreply.github.com>
2019-08-22 04:53:38 +0000

    Add files via upload

--- categoria_produto.py
+++ categoria_produto.py
@@ -1,14 +1,11 @@
-
-
 class CategoriaProduto:
-
     def __init__(self, titulo):
         self.__titulo = titulo
 
     @property
     def titulo(self):
-        return self.__titulo 
-        
+        return self.__titulo
+
     @titulo.setter
-    def titulo(self,titulo):
+    def titulo(self, titulo):
         self.__titulo = titulo
