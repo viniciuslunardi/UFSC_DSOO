1b51d70cd28d7eed988d797891430d8c109adc44
Rafael Nilson Witt <34680072+rafaelwitter@users.noreply.github.com>
2019-08-23 20:36:14 -0300

    Add files via upload

--- Biblioteca_Exercicio4/editora.py
+++ Biblioteca_Exercicio4/editora.py
@@ -1,8 +1,19 @@
 class Editora:
     def __init__(self, codigo: int, nome: str):
-        ...
+        self.__codigo = codigo
+        self.__nome = nome
 
-    ... Adicionar demais getters
+    @property
+    def codigo(self):
+        return self.__codigo
+    @codigo.setter
+    def codigo(self,codigo):
+        self.__codigo = codigo
 
-
-    ... Adicionar demais setters
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome
+    
