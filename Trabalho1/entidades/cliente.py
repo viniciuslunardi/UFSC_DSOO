5d60dd1ce3ea92409a40d549e9de1ff39da8e52a
Rafael Nilson Witt <34680072+rafaelwitter@users.noreply.github.com>
2019-09-25 18:29:27 -0300

    Add files via upload

---
+++ Trabalho1/entidades/cliente.py
@@ -0,0 +1,24 @@
+from pessoa import Pessoa
+from endereco import Endereco
+class Cliente(Pessoa):
+    def __init__(self, nome: str, cpf: str, telefone: str, endereco: Endereco, 
+        pedidos_anteriores: [], fidelidade: int=0):
+        super().__init__(nome, cpf, telefone)
+        self.__endereco = endereco
+        self.__pedidos_anteriores = pedidos_anteriores
+        self.__fidelidade = fidelidade
+    
+    @property
+    def endereco(self):
+        return self.__endereco
+    
+    @property
+    def fidelidade(self):
+        return self.__fidelidade
+    
+    @endereco.setter
+    def endereco(self, endereco: Endereco):
+        if (endereco is not None) and (isinstance(endereco, Endereco)):
+            self.__endereco = endereco
+
+
